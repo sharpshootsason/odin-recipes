7f5edf6cd340584c2d609216e690f3d1dc92f53a
sharpshootsason <sakshi.rakesh.gosain@gmail.com>
2023-03-01 15:13:52 -0500

    bleh

---
+++ java.js
@@ -0,0 +1,27 @@
+/*write a function called add7 that takes one number and returns that number + 7 */
+
+function add7(num) {
+    console.log(num + 7);
+}
+
+add7(1); 
+
+
+/* write a function called multiply that takes 2 numbers and treturns their product */
+function multiply(a,b) {
+    console.log(a * b);
+}
+
+multiply (3,4);
+
+
+/* write a function called capitalize that takes a string and returns that string with only the first letter capitalized */
+
+function capitalize() {
+    return string.charAt(0).toUppercase() + string.slice(1).toLowercase();
+}
+
+let string = ('sakshi');
+let result = capitalize(string);
+
+console.log(result);
