9d4cf8c94f4eecb0945f5f442030f02b01f22e82
sharpshootsason <sakshi.rakesh.gosain@gmail.com>
2023-03-04 20:28:23 -0500

    fizzbuzz or bust

--- java.js
+++ java.js
@@ -17,11 +17,38 @@ multiply (3,4);
 
 /* write a function called capitalize that takes a string and returns that string with only the first letter capitalized */
 
-function capitalize() {
-    return string.charAt(0).toUppercase() + string.slice(1).toLowercase();
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
 
-let string = ('sakshi');
-let result = capitalize(string);
+result = capitalize('sakshi');
 
 console.log(result);
+
+
+function lastletter(string) {
+    return string.slice(5,6);
+}
+
+result = lastletter('sakshi')
+
+console.log(result)
+
+
+
+/* fizz buzz practice */
+
+
+let answer = parseInt(prompt("Please enter the number you would like to Fizzbuzz up to:"))
+
+for (let i = 1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
